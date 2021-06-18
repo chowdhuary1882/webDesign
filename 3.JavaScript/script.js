b96c8b40e6ace5b2633cc2714cc20e24e7244f09
Arif <chowdhuary1882@gmail.com>
2021-06-18 04:37:03 -0400

    Checking Vowels with switch statement

--- 3.JavaScript/script.js
+++ 3.JavaScript/script.js
@@ -1,3 +1,4 @@
+// Compare two numbers
 var number1 = prompt("Enter the first number : ");
 var number2 = prompt("Enter the last nunmber : ");
 
@@ -7,3 +8,20 @@ else if (number2 > number1)
   console.log("Last number is greater than First number");
 else
   console.log("Both numbers are equal");
+
+
+//Checking Vowels
+
+var letter = prompt("Enter a alphabetc : ");
+letter = letter.toLowerCase();
+switch (letter) {
+  case 'a':
+  case 'e':
+  case 'i':
+  case 'o':
+  case 'u':
+    console.log("The letter is Vowel");
+    break;
+  default:
+    console.log("The letter is consonant");
+}
