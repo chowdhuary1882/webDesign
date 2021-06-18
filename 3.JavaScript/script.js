72b5f45aceb6c2e263cf74ea228027b6168a079f
Arif <chowdhuary1882@gmail.com>
2021-06-18 04:25:22 -0400

    compare two numbers

--- 3.JavaScript/script.js
+++ 3.JavaScript/script.js
@@ -1,1 +1,9 @@
-var name = "arif";
+var number1 = prompt("Enter the first number : ");
+var number2 = prompt("Enter the last nunmber : ");
+
+if (number1 > number2)
+  console.log("First number is greater than Last number");
+else if (number2 > number1)
+  console.log("Last number is greater than First number");
+else
+  console.log("Both numbers are equal");
